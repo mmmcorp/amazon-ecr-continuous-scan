5dabc652826667dee15ea769194b604bc599af33
x-blood <sato@mmmcorp.co.jp>
2021-03-08 18:10:07 +0900

    Apply IP address restriction to API Gateway.

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -3,5 +3,15 @@ env GOOS=linux GOARCH=amd64 go build -v -ldflags '-d -s -w' -a -tags netgo -inst
 env GOOS=linux GOARCH=amd64 go build -v -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo -o bin/start-scan ./start-scan
 env GOOS=linux GOARCH=amd64 go build -v -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo -o bin/summary ./summary
 env GOOS=linux GOARCH=amd64 go build -v -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo -o bin/findings ./findings
-sam package --template-file template.yaml --output-template-file current-stack.yaml --s3-bucket ${ECR_SCAN_SVC_BUCKET}
-sam deploy --template-file current-stack.yaml --stack-name ${ECR_SCAN_STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides ConfigBucketName="${ECR_SCAN_CONFIG_BUCKET}"
+
+sam package \
+--template-file template.yaml \
+--output-template-file current-stack.yaml \
+--s3-bucket ${ECR_SCAN_SVC_BUCKET} \
+--s3-prefix ${ECR_SCAN_SVC_S3_PREFIX}
+
+sam deploy \
+--template-file current-stack.yaml \
+--stack-name ${ECR_SCAN_STACK_NAME} \
+--capabilities CAPABILITY_IAM \
+--parameter-overrides ConfigBucketName="${ECR_SCAN_CONFIG_BUCKET}" AllowIPAddress="${ALLOW_IP_ADDRESS}"
